e74ad528ecbfab73c0c1e4353fdd7c14b606b446
Mariano Gappa <spinetta@gmail.com>
2022-03-12 18:18:35 +0000

    Allow binary to run API/Daemon/BackOffice separately.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -18,11 +19,22 @@ import (
 
 var (
 	//go:embed public/*
-	files embed.FS
+	files          embed.FS
+	flagAPI        = flag.Bool("api", false, "only run API")
+	flagBackOffice = flag.Bool("backoffice", false, "only run Back Office")
+	flagDaemon     = flag.Bool("daemon", false, "only run Daemon")
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	flag.Parse()
+
+	var (
+		runAll        = (!*flagAPI && !*flagBackOffice && !*flagDaemon) || (*flagAPI && *flagBackOffice && *flagDaemon)
+		runAPI        = runAll || *flagAPI
+		runBackOffice = runAll || *flagBackOffice
+		runDaemon     = runAll || *flagDaemon
+	)
 
 	if os.Getenv("PREDICTIONS_TWITTER_BEARER_TOKEN") == "" || os.Getenv("PREDICTIONS_YOUTUBE_API_KEY") == "" {
 		log.Fatal(
@@ -32,27 +44,47 @@ func main() {
 		)
 	}
 
+	if runBackOffice && !runAPI && os.Getenv("PREDICTIONS_API_PORT") == "" && os.Getenv("PREDICTIONS_API_URL") == "" {
+		log.Fatal(`If you want to run the Back Office but not the API, please specify either PREDICTIONS_API_URL or ` +
+			"PREDICTIONS_API_PORT. Otherwise I don't know how to reach the API.")
+	}
+
 	var (
 		postgresDBStorage = statestorage.MustNewPostgresDBStateStorage()
 		market            = market.NewMarket()
 		metadataFetcher   = metadatafetcher.NewMetadataFetcher()
 		api               = api.NewAPI(market, postgresDBStorage, *metadataFetcher)
-		backgroundRunner  = daemon.NewDaemon(market, postgresDBStorage)
+		daemon            = daemon.NewDaemon(market, postgresDBStorage)
 		backOffice        = backoffice.NewBackOfficeUI(files)
 
-		apiPort                  = envOrInt("PREDICTIONS_API_PORT", 2345)
-		apiUrl                   = fmt.Sprintf("http://localhost:%v", apiPort)
-		backOfficePort           = envOrInt("PREDICTIONS_BACKOFFICE_PORT", 1234)
-		backgroundRunnerDuration = envOrDur("PREDICTIONS_BACKGROUND_RUNNER_DURATION", 10*time.Second)
+		apiPort        = envOrInt("PREDICTIONS_API_PORT", 2345)
+		apiUrl         = envOrStr("PREDICTIONS_API_URL", fmt.Sprintf("http://localhost:%v", apiPort))
+		backOfficePort = envOrInt("PREDICTIONS_BACKOFFICE_PORT", 1234)
+		daemonDuration = envOrDur("PREDICTIONS_DAEMON_DURATION", 10*time.Second)
 	)
 
-	go api.MustBlockinglyListenAndServe(apiUrl)
-	go backOffice.MustBlockinglyServe(backOfficePort, apiUrl)
-	go backgroundRunner.BlockinglyRunEvery(backgroundRunnerDuration)
+	if runAPI {
+		go api.MustBlockinglyListenAndServe(apiUrl)
+	}
+
+	if runBackOffice {
+		go backOffice.MustBlockinglyServe(backOfficePort, apiUrl)
+	}
+
+	if runDaemon {
+		go daemon.BlockinglyRunEvery(daemonDuration)
+	}
 
 	select {}
 }
 
+func envOrStr(env, or string) string {
+	s := os.Getenv(env)
+	if s == "" {
+		return or
+	}
+	return s
+}
 func envOrInt(s string, or int) int {
 	i, err := strconv.Atoi(os.Getenv(s))
 	if err != nil {
